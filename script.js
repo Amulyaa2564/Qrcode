6a75b461b082903950e97dfd610c4bf78f3752e1
Amulyaa2564 <amulyagathala@gmail.com>
2025-11-19 13:22:37 +0530

    Updated javascript code

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let inputText=document.getElementById('inputText');
 let errorMsg=document.getElementById('errorMsg');
 let submitBtn=document.getElementById('submitBtn');
+let qrDiv=document.getElementById('qrDiv');
+let qrImage=document.getElementById('qrImage');
 
-submitBtn.onclick=function(){
+function addQr(){
     if(inputText.value.trim()===""){
-        errorMsg.textContent="Please enter text or URL"
+        errorMsg.textContent="Please enter text or URL";
     }else{
         errorMsg.textContent="";
+        qrImage.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= "+inputText.value;
+        inputText.value="";
     }
 }
+
+inputText.addEventListener('keydown',function(event){
+    if(event.key==='Enter'){
+        addQr();
+    }
+});
+
+submitBtn.onclick=addQr;
